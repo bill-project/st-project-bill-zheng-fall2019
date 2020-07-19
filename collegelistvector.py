c8113d71f63696840a54b2060fe1d023f274f058
Bill Zheng <31973301+BILLLLLLL@users.noreply.github.com>
2020-07-18 21:05:19 -0700

    Create collegelistvector.py
    This is the standard vector for a college, used for college calculator and other machine learning uses

---
+++ collegelistvector.py
@@ -0,0 +1,6 @@
+//
+
+//
+
+Harvardvector = [4.04, 1470, 1570, 33, 35, 10]
+#Goes like this: Avg. GPA, 25th percentile SAT, 75th percentile SAT, 25th percentile ACT, 75th percentile ACT, time to repay loan, 
