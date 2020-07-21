d7e54297d16c5d636d46c716e42b7ad63a5a6285
Jung Choi <jungchoi2023@gmail.com>
2020-07-20 19:19:50 -0700

    Added USC to the collegelistvector.py

--- collegelistvector.py
+++ collegelistvector.py
@@ -1,62 +1,91 @@
-#Goes like this: Avg. GPA, 25th percentile SAT, 75th percentile SAT, 25th percentile ACT, 75th percentile ACT, time to repay loan
-#HarvardvectorMajors goes like this: best major 1, best major 2... best major N
+
+# Goes like this: Avg. GPA, 25th percentile SAT, 75th percentile SAT, 25th percentile ACT, 75th percentile ACT, time to repay loan
+# HarvardvectorMajors goes like this: best major 1, best major 2... best major N
 
 Harvardva = [4.04, 1470, 1570, 33, 35, 10]
 Harvardvm = [""]
-USCva = []
-USCvm = []
+
+# USC allows up to 25 years to repay loans technically
+USCva = [3.79, 1350, 1530, 30, 34, 10]
+USCvm = [""]
+
 ASUva = []
 ASUvm = []
 
 Dukeva = []
 Dukevm = []
+
 UPennva = []
 UPennvm = []
+
 UChicagova = []
 UChicagovm = []
+
 Yaleva = []
 Yalevm = []
+
 Princetonva = []
 Princetonvm = []
+
 Brownva = []
 Brownvm = []
+
 Dartmouthva = []
 Dartmouthvm = []
+
 NYUva = []
 NYUvm = []
+
 UCLAva = []
 UCLAvm = []
+
 UCIva = []
 UCIvm = []
+
 UIUCva = []
 UIUCvm = []
+
 UCSCva = []
 UCSCvm = []
+
 UCSDva = []
 UCSDvm = []
+
 UCSBva = []
 UCSBvm = []
+
 UCDva = []
 UCDvm = []
+
 UCSFva = []
 UCSFvm = []
+
 UCBva = []
 UCBvm = []
-UCRva= [] 
+
+UCRva= []
 UCRvm = []
+
 UCMva = []
 UCMvm = []
+
 CSUBakersfieldva = []
 CSUBakersfieldvm = []
+
 CSUChannelIslandsva = []
 CSUchannelIslandsvm = []
+
 Chicostateva = []
 Chicostatevm = []
+
 CSUDominguezhillsva = []
 CSUDominguezhillsvm = []
+
 CSUEastbayva = []
 CSUEastbayvm = []
+
 Fresnostateva = []
 Fresnostatevm = []
+
 CSUFullertonva = []
 CSUFullertonvm = []
