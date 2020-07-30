f1f088e3bf083bd3767f0c313ac6f0c9990731f5
Bill Zheng <31973301+BILLLLLLL@users.noreply.github.com>
2020-07-29 22:15:55 -0700

    Update ML.py
    This algorithm is working, trying to refine it a bit more.

--- ML.py
+++ ML.py
@@ -3,8 +3,10 @@ from sklearn import svm
 from sklearn import linear_model
 from flask import Flask
 from flask_cors import CORS
+import data1
 #import json
 import pickle
+
 #import random
 
 app = Flask(__name__)
@@ -17,33 +19,33 @@ Major list (Output1) & strength (Output2)
 	1: Physics   1: Strong
 	2: Psych     2: Medium
 	3: Chem      3: Weak
-Be added in main.py when finished
 '''
 filename = 'model_source_major.sav'
 filename2 = "cv_model_vectorizer.pickle_major"
 
 def major_training():
-	input_data = [[4, 4, 5, 5, 1], [0, 0, 4, 2, 2], [5, 2, 3, 4, 2], [2, 2, 5, 4, 1], [5, 5, 0, 0, 1]]
+	input_data = ['score1', 'score2', 'score3', 'score4', 'score5', 'score6', 'score7']
 	cv = CountVectorizer()
 	input_ = cv.fit_transform(input_data).toarray()
-	output1 = [1, 1, 2, 1, 3]
+	output1 = [1, 1, 2, 1, 3, 2, 2]
 	model = svm.SVC()
 	model.fit(input_, output1)
 	filename = 'model_source_major.sav'
-	filename2 = "cv_model_vectorizer.pickle"
+	filename2 = "cv_model_vectorizer.sav"
 	pickle.dump(model, open(filename, 'wb'))
 	pickle.dump(cv, open(filename2, "wb"))
 
-
+testML = ['3, 2, 0, 0, 2']
 @app.route("/majorfit/<profile>")
 def major_prediction(profile):
-	test = [profile]
+	test = profile
 	filename = 'model_source_major.sav'
-	filename2 = "cv_model_vectorizer.pickle"
+	filename2 = "cv_model_vectorizer.sav"
 	loaded_model = pickle.load(open(filename, 'rb'))
 	cv = pickle.load(open(filename2, "rb"))
 	majorresult = loaded_model.predict(cv.transform(test).toarray())
 	print(majorresult)
 	return(str(majorresult[0]))
 
-major_prediction([3, 3, 4, 4, 2])
+major_training()
+major_prediction(testML)
